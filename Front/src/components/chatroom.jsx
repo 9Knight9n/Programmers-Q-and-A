ddfc112ba95ea10480a83d3b4a9e1b506a77948e
Mohamad Sajad Naghizadeh <naghizadeh.mdsd@gmail.com>
2020-10-28 22:28:22 +0330

    beta version for homepage

--- Front/src/components/chatroom.jsx
+++ Front/src/components/chatroom.jsx
@@ -5,12 +5,70 @@ class Chatroom extends Component {
     state = {  }
     render() { 
         return (  
-            <div>
-                <div className="chatbox">
-                    chat
+            <React.Fragment>
+            <div className="chatbox">
+                <div className="mt-4 mb-7 mr-5">
+                    <div>
+                        <ul class="simple-nested">
+                        <li>
+                            <div class="comment">
+                                <p><a href="">ali</a></p>
+                                <em>Feb 23rd</em>
+                                <p>this is general comment.</p>
+                                <p><a href="">reply</a></p>
+                            </div>
+                            <ul>
+                                <li>
+                                    <div class="comment">
+                                        <p><a href="">zahra</a></p>
+                                        <em>Feb 23rd</em>
+                                        <p>this is first reply comment.</p>
+                                        <p><a href="">reply</a></p>
+                                    </div>
+                                    <ul>
+                                        <li>
+                                            <div class="comment">
+                                                <p><a href="">reza</a></p>
+                                                <em>Feb 23rd</em>
+                                                <p>this is second reply comment..</p>
+                                                <p><a href="">reply</a></p>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                <div class="comment">
+                                        <p><a href="">Ali</a></p>
+                                        <em>Feb 23rd</em>
+                                        <p>this is a reply to initial comment.</p>
+                                        <p><a href="">reply</a></p>
+                                    </div>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <div class="comment">
+                                <p><a href="">maryam </a></p>
+                                <em>Feb 23rd</em>
+                                <p>this is third reply comment..</p>
+                                <p><a href="">reply</a></p>
+                            </div>
+                        </li>
+                            <li>
+                            <div class="comment">
+                                <p><a href="">maryam </a></p>
+                                <em>Feb 23rd</em>
+                                <p>this is forth reply comment..</p>
+                                <p><a href="">reply</a></p>
+                            </div>
+                        </li>
+                    </ul>
+                    </div>
+                    
                 </div>
-                <button className="btn btn-primary align-right">Submit Question</button>
             </div>
+            {/* <button className="btn btn-primary ml-10">Submit Question</button> */}
+            </React.Fragment>
         );
     }
 }
--- Front/src/components/homepage.jsx
+++ Front/src/components/homepage.jsx
@@ -1,4 +1,7 @@
 //Coded by Sajad
+//initial check done
+//tasks remaining :
+//1.connecting to backend and reading chatroom list
 
 import React, { Component } from 'react';
 import LeftMenu from './leftMenu';
@@ -9,10 +12,13 @@ import j_logo from "../img/java-logo.png";
 import p_logo from "../img/python-logo.png";
 
 class Homepage extends Component {
-
+//chatrooms id must be non negetive
     state = {
-        activeChatroom : <div>Select a Chatroom</div>,
-        options:[
+        activeChatroom : 
+            <div className="black-text my-auto">
+                    Select a Chatroom
+            </div>,
+        chatrooms:[
             {
                 id:1,
                 ButtonName:'Chat Room 1',
@@ -23,24 +29,100 @@ class Homepage extends Component {
                 ButtonName:'Chat Room 2',
                 img:p_logo
     
+            },
+            {
+                id:3,
+                ButtonName:'Chat Room 1',
+                img:j_logo
+            },
+            {
+                id:4,
+                ButtonName:'Chat Room 2',
+                img:p_logo
+    
+            },
+            {
+                id:5,
+                ButtonName:'Chat Room 1',
+                img:j_logo
+            },
+            {
+                id:6,
+                ButtonName:'Chat Room 2',
+                img:p_logo
+    
+            },
+            {
+                id:7,
+                ButtonName:'Chat Room 1',
+                img:j_logo
+            },
+            {
+                id:8,
+                ButtonName:'Chat Room 2',
+                img:p_logo
+    
+            },
+            {
+                id:9,
+                ButtonName:'Chat Room 1',
+                img:j_logo
+            },
+            {
+                id:10,
+                ButtonName:'Chat Room 2',
+                img:p_logo
+    
+            },
+            {
+                id:11,
+                ButtonName:'Chat Room 1',
+                img:j_logo
+            },
+            {
+                id:12,
+                ButtonName:'Chat Room 2',
+                img:p_logo
+    
+            },
+            {
+                id:13,
+                ButtonName:'Chat Room 1',
+                img:j_logo
+            },
+            {
+                id:14,
+                ButtonName:'Chat Room 2',
+                img:p_logo
+    
+            },
+            {
+                id:15,
+                ButtonName:'Chat Room 1',
+                img:j_logo
+            },
+            {
+                id:16,
+                ButtonName:'Chat Room 2',
+                img:p_logo
+    
             }
         ]
     }
 
 
-    tabClicked = (id) =>{
-        let activeChatroom = <div>empty</div>
-        let options = [...this.state.options];
-        for (let i = 0; i < options.length; i++)
-            if (options[i].id===id)
+    chatroomClicked = (id) =>{
+        let chatrooms = [...this.state.chatrooms];
+        for (let i = 0; i < chatrooms.length; i++)
+            if (chatrooms[i].id===id)
             {
-                options[i].isActive = true;
-                activeChatroom = <Chatroom chatID={options[i].id}/>
+                chatrooms[i].isActive = true;
+                let activeChatroom = <Chatroom chatID={chatrooms[i].id}/>
                 this.setState({activeChatroom:activeChatroom});
             }
             else
-                options[i].isActive = false;
-        this.setState({options});      
+                chatrooms[i].isActive = false;
+        this.setState({chatrooms});      
     }
 
 
@@ -48,7 +130,7 @@ class Homepage extends Component {
         return (
             <React.Fragment>
                 <div className="LeftColumn">
-                    <LeftMenu tabs={this.state.options} onTabClicked={this.tabClicked}/>
+                    <LeftMenu chatrooms={this.state.chatrooms} chatroomClicked={this.chatroomClicked}/>
                 </div>
                 <div className="RightColumn">
                     <Navbar />
