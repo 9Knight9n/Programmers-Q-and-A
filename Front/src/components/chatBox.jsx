a6cf61810e3017dfaefe4f555bbd3cf4fc390e8a
parisa <parisazafari79@gmail.com>
2020-10-31 12:14:56 +0330

    change chatbox

--- Front/src/components/chatBox.jsx
+++ Front/src/components/chatBox.jsx
@@ -6,16 +6,16 @@ class ChatBox extends Component {
     render() { 
         return (
                 <div className="comment d-flex flex-column">
-                        <div className="d-flex align-items-center"><img  id="profile-img-in-chat" src="http://emilcarlsson.se/assets/mikeross.png" /></div>
+                    <div className="d-flex flex-row">
+                        <div className="d-flex align-items-center mr-2"><img  id="profile-img-in-chat" src="http://emilcarlsson.se/assets/mikeross.png" /></div>
                         <h1 className="d-flex align-items-center"><p><a href="">{this.props.chat.by}</a></p></h1>
-                    
-
-                    <p>{this.props.chat.content}</p>
+                    </div>
+                    <div className="m-3"><p>{this.props.chat.content}</p></div>
                     <div className="row">
-                        <div class="col-lg-6 d-flex justify-content-start">
+                        <div className="col-lg-6 d-flex justify-content-start">
                             <em>{this.props.chat.time}</em>
                         </div>
-                        <div class="col-lg-6 d-flex justify-content-end">
+                        <div className="col-lg-6 d-flex justify-content-end">
                             <p>
                                 <a onClick={() => this.props.refToChatroom.current.clicked(this.props.chat.id)} href="#">
                                     reply
