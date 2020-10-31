e90af0147c4bd3e31e28dba521ceaf6fc43391c1
sadeghjafari5528 <sadeghjafari5528@gmail.com>
2020-10-31 20:58:02 +0330

    make chat_text model

--- mysite/submittext/models.py
+++ mysite/submittext/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from registeration.models import User
 # Create your models here.
+class chat_text(models.Model):
+    user = models.ForeignKey(User , on_delete=models.CASCADE)
+    parent_text = models.ForeignKey('self' , on_delete=models.CASCADE , null=True)
+    text = models.TextField()
+    
