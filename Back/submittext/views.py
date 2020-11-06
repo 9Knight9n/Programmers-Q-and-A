c1ec84ceaaf0623f41b01f3876d60adb7d6452b4
sadeghjafari5528 <sadeghjafari5528@gmail.com>
2020-11-06 15:26:00 +0330

    start again debt

--- Back/submittext/views.py
+++ Back/submittext/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import chat_text
+from .models import ChatText
 from django.views.decorators.csrf import csrf_exempt
 from registeration.models import User , UserManager
 from django.http import HttpResponse
@@ -8,7 +8,7 @@ from django.core import serializers
 
 def submit_text(request):
     if request.method == 'GET':
-        response_data = chat_text.objects.all()
+        response_data = ChatText.objects.all()
         tmpJson = serializers.serialize("json",response_data)
         tmpObj = json.loads(tmpJson)
         return HttpResponse(json.dumps(tmpObj))
