51197a6b7361153b575bdbeb2f79e723232fe054
Mohamad Sajad Naghizadeh <naghizadeh.mdsd@gmail.com>
2020-11-07 20:24:28 +0330

    Preview added

--- Front/src/App.js
+++ Front/src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import SignUpForm from './pages/SignUpForm';
 import SignInForm from './pages/SignInForm';
 import Homepage from './components/homepage';
+import SelectAvatar from './components/selectAvatar';
 import './index.css';
 
 
 
 class App extends Component {
   state={
-    currentComponent:<SignInForm refToSelectComponent={this.selectComponent.bind(this)}/>
+    currentComponent:<SelectAvatar src={require("./img/default-profile-picture.jpg")} side="50"/>
   }
 
   selectComponent(index){
---
+++ Front/src/components/selectAvatar.jsx
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import './CSS/selectAvatar.css';
+import Avatar from 'react-avatar-edit'
+
+class SelectAvatar extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+          preview: null,
+          src : this.props.src,
+          showSelect:false
+        }
+        this.onCrop = this.onCrop.bind(this)
+        this.onClose = this.onClose.bind(this)
+        this.hover = this.hover.bind(this)
+    }
+    onClose() {
+        this.setState({preview: null})
+    }
+      
+    onCrop(preview) {
+        this.setState({preview})
+    }
+
+    hover(isHover){
+        this.setState({showSelect:isHover})
+    }
+
+
+
+    render() { 
+        let style1;
+        if(!this.state.showSelect)
+            style1 = {
+                height:this.props.side+"vh",
+                width:this.props.side+"vh",
+                backgroundImage:"url(" +  this.state.src  + ")",
+                
+            }
+        else
+            style1 = {
+                height:this.props.side+"vh",
+                width:this.props.side+"vh",
+                backgroundImage:"url(" +  this.state.src  + ")",
+                opacity: 0.25,
+            }
+        return ( 
+            <div className="box">
+                <div onMouseEnter={() =>this.hover(true)}
+                    onMouseLeave={() => this.hover(false)}
+                    id="preview" 
+                    style={style1}>
+                    
+                        {/* <Avatar
+        width={450}
+        height={350}
+        onCrop={this.onCrop}
+        onClose={this.onClose}
+        src={this.state.src}
+    />
+                        <img src={this.state.preview} alt="Preview" /> */}
+
+                </div>
+                
+                {this.state.showSelect ? 
+                        <button 
+                        onMouseEnter={() =>this.hover(true)}
+                        onMouseLeave={() => this.hover(false)}
+                         className="select-btn">
+                            <svg width="1em" height="1em" viewBox="0 0 16 16" className="mr-2 bi bi-pencil-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
+                            </svg>
+                            Change picture
+                        </button>
+                    :""}
+
+                    
+            </div>
+                
+
+
+        );
+    }
+}
+ 
+export default SelectAvatar;
