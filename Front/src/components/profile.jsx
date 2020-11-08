b191ee6378b232a8bd5ab4dad44bd8b6e7765518
parisa <parisazafari79@gmail.com>
2020-11-08 20:53:33 +0330

    edit css profile

--- Front/src/components/profile.jsx
+++ Front/src/components/profile.jsx
@@ -3,29 +3,36 @@ import './CSS/profile.css';
 
 
 
+
+
+
+
+
 class Profile extends Component {
     state = {  }
     render() { 
         return ( 
             <React.Fragment>
+                
                 <div class="profile">
-                    <div class="content-form">  
+                  
+                    <div class="content-form">
+                       
                         <div class="INPUT-FORM"> 
-                            <input type="text" class="input" placeholder="full name"/><br></br>
-                            <br></br><br></br><br></br><br></br><br></br><br></br>
-                            <input type="text" class="input" placeholder="username"/><br></br>
-                            <input type="text" class="input" placeholder="phone"/><br></br>
+
+                            <input type="text" class="input" placeholder="First name"/><br></br>
+                            <input type="text" class="input" placeholder="Last name"/><br></br>
+                            <input type="text" class="input" placeholder="User name"/><br></br>
                             <input type="text" class="input" placeholder="Email"/><br></br>
-                            <input type="text" class="input" placeholder="subject"/><br></br>
-                            <br></br><br></br>
+                            <div class="btn">Save all details</div>
             
                             </div>
-                            <div class="msg">
-                            <textarea placeholder="Message"></textarea><br></br>
-                            <div class="btn">send</div>
+                            
+                            
+                            
                             </div>
                     </div> 
-                </div>
+                
             </React.Fragment>
             
          );
