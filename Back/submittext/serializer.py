571781cc58aa2131c3784d6b8d3564b81d73061a
sadeghjafari5528 <sadeghjafari5528@gmail.com>
2020-11-08 20:06:31 +0330

    try to fix submit authentication

--- Back/submittext/serializer.py
+++ Back/submittext/serializer.py
@@ -6,3 +6,7 @@ class ChatTextSerializer(serializers.ModelSerializer):
     class Meta:
         model = ChatText
         fields = ['id' , 'user', 'parent_text', 'text' , 'time']
+
+    def ShowText(self):
+        data = ChatText.objects.all()
+        return data
--- Back/submittext/views.py
+++ Back/submittext/views.py
@@ -1,14 +1,16 @@
-from django.shortcuts import render
-from .models import ChatText
-from django.views.decorators.csrf import csrf_exempt
-from registeration.models import User , UserManager
-from django.http import HttpResponse
-import json
-from django.core import serializers
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-def submit_text(request):
+from .serializer import ChatTextSerializer
+from .models import ChatText
+
+@api_view(['GET' , ])
+def send_all_text(request):
     if request.method == 'GET':
-        response_data = ChatText.objects.all()
-        tmpJson = serializers.serialize("json",response_data)
-        tmpObj = json.loads(tmpJson)
-        return HttpResponse(json.dumps(tmpObj))
+        chattext = ChatText.objects.all()
+        serializer = ChatTextSerializer(chattext)
+        return Response(serializer.data)
+
+def get_text(request):
+    pass
