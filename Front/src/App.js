89de6bbf054bab70d07f1d98c3c54337fee6895d
Mohamad Sajad Naghizadeh <naghizadeh.mdsd@gmail.com>
2020-11-09 10:15:45 +0330

    Fixed remaining bugs
    Component is ready to be used in Chatroom and profile

--- Front/src/App.js
+++ Front/src/App.js
@@ -8,8 +8,39 @@ import './index.css';
 
 
 class App extends Component {
-  state={
-    currentComponent:<SelectAvatar src={require("./img/default-profile-picture.jpg")} side="50"/>
+
+  constructor(props) {
+    super(props)
+    const src = require('./img/default-profile-picture.jpg');
+    this.state = {
+      src,
+      preview:null,
+      avatarChanged:false,
+      currentComponent:<React.Fragment>
+        
+      </React.Fragment>
+    }
+    this.onCrop = this.onCrop.bind(this)
+    this.onClose = this.onClose.bind(this)
+    this.onSave = this.onSave.bind(this)
+  }
+
+
+  onClose() {
+    this.setState({preview: null})
+  }
+  
+  onCrop(preview) {
+    this.setState({preview})
+    console.log(preview)
+  }
+
+  onSave(){
+    let src = this.state.preview
+    console.log("save button pressed")
+    console.log("current preview:",src)
+    this.setState({avatarChanged:true,src})
+    console.log("changed src:",this.state.src)
   }
 
   selectComponent(index){
@@ -24,7 +55,11 @@ class App extends Component {
   render(){
     return (
       <React.Fragment>
-        {this.state.currentComponent}
+        <SelectAvatar src={this.state.src}
+        onCrop={this.onCrop}
+        onClose={this.onClose}
+        onSave={this.onSave} side="50" />
+        <img src={this.state.src} alt="Preview" />
       </React.Fragment>
     ); 
   }
