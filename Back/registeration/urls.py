85555e327e349b9efa1314f2658ab5b100cb8f2e
sadeghjafari5528 <sadeghjafari5528@gmail.com>
2020-11-10 15:59:36 +0330

    Sure that signin token work currenctly

--- Back/registeration/urls.py
+++ Back/registeration/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from registeration import views
 
 urlpatterns = [
-    path("signin", views.signin, name="signin"),
+    path("", views.signin, name="signin"),
     path("signup", views.signup, name="signup"),
 ]
--- Back/registeration/views.py
+++ Back/registeration/views.py
@@ -2,6 +2,7 @@ from rest_framework.decorators import api_view, permission_classes
 from .serializer import AccountRegistrationSerializer, UserProfileSerializer
 from rest_framework.response import Response
 from rest_framework import status
+from .models import User
 
 
 @api_view(['POST'])
@@ -11,11 +12,18 @@ def signup(request):
     if serializer.is_valid():
         serializer.save()
         return Response({'message': 'New user created'}, status=status.HTTP_201_CREATED)
-    return Response({'message':'user with this email address already exists.'}, status=status.HTTP_400_BAD_REQUEST)
+    return Response({'message':'user with this email address already exists.'}, status=status.HTTP_404_NOT_FOUND)
 
 
 @api_view(['POST'])
 def signin(request):
-    serializer = UserProfileSerializer(request.user)
-    print(serializer.data)
-    return Response({'user': serializer.data}, status=status.HTTP_200_OK)
+    if request.method == 'POST':
+        post_data = dict(request.POST)
+        check_user = User.objects.filter(email=post_data['email'][0])
+        if len(check_user) == 0:
+            return Response({"token":"this email does not exist!"} , status=status.HTTP_404_NOT_FOUND)
+        else:
+            if check_user[0].check_password(post_data['password'][0]):
+                return Response({"error":"wellcome"} , status=status.HTTP_202_ACCEPTED)
+            else:
+                return Response({"error": "password or email is not correct"} , status=status.HTTP_406_NOT_ACCEPTABLE)
