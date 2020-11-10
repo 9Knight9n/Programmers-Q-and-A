96e1748f1b17768cf2a9403f868b085588eb9c2a
abed ebadi <73102057+abed-eb@users.noreply.github.com>
2020-11-10 21:02:32 +0330

    changed back

--- Front/src/components/ChatroomCreationFirst.js
+++ Front/src/components/ChatroomCreationFirst.js
@@ -1,4 +1,10 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './CSS/ChatroomCreation.css';
 import osImg from '../img/operating-system.png';
 import appImg from '../img/mobile-app.png';
@@ -8,39 +14,49 @@ class ChatroomCreationOs extends Component {
     state = {  }
     render() { 
         return ( 
-            <div class="main-box">
-                <div class="elements">
-                    <div class="contextKinds">
-                        <label for="contextKinds">Choose a context for your chatroom:</label>
-                        <div className="contextLogo">
-                            <div className="osBox">
-                                <div className="os">
-                                    <a href="#">
-                                        <img src={osImg} />
-                                        <h3>Operating system</h3>
-                                    </a> 
+            <Router>
+                <div class="main-box">
+                    <div class="elements">
+                        <div class="contextKinds">
+                            <label for="contextKinds"><h1>Choose a context for your chatroom:</h1></label>
+                            <div className="contextLogo">
+                                <div className="osBox">
+                                    <div className="os">
+                                        <Link to="/chatroomCreationOs">
+                                            <img src={osImg} />
+                                            <h3>Operating system</h3>
+                                        </Link> 
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="appBox">
-                                <div className="application">
-                                    <a href="#">
-                                        <img src={appImg} />
-                                        <h3>Application</h3>
-                                    </a> 
+                                <div className="appBox">
+                                    <div className="application">
+                                        <Link to="/chatroomCreationApp">
+                                            <img src={appImg} />
+                                            <h3>Application</h3>
+                                        </Link> 
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="prLangBox">
-                                <div className="prlang">
-                                    <a href="#">
-                                        <img src={prLangImg} />
-                                        <h3>Programming Languages</h3>
-                                    </a>  
+                                <div className="prLangBox">
+                                    <div className="prlang">
+                                        <a href="#">
+                                            <img src={prLangImg} />
+                                            <h3>Programming Languages</h3>
+                                        </a>  
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
+                    <Switch>
+                        <Route path="/chatroomCreationOs">
+                            <ChatroomCreationOs />
+                        </Route>
+                        <Route path="/chatroomCreationApp">
+                            <chatroomCreationApp />
+                        </Route>
+                    </Switch>
                 </div>
-            </div>
+            </Router>
          );
     }
 }
