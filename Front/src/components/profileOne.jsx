f6eb855fa74a7a7774837fbecfaabd94cb5ec4e1
parisa <parisazafari79@gmail.com>
2020-11-14 15:05:49 +0330

    End pro css

--- Front/src/components/profileOne.jsx
+++ Front/src/components/profileOne.jsx
@@ -4,10 +4,22 @@ import './CSS/profileOne.css';
 
 class ProfileOne extends Component {
     state = {
-        usernameError:false,
-        emailError:false,
-        succeed:false
-    }
+                usernameError:false,
+                emailError:false,
+                succeed:false,
+                firstName: null,
+                lastName:null,
+                username:null,
+                emailProfile:null
+            }
+            handleChange = (e) => { 
+                let target=e.target;
+                let name = target.name;
+                let value = target.value
+                this.setState({
+                 [name]:value
+                });
+              }
     render() { 
         return ( 
             <React.Fragment>
@@ -17,14 +29,14 @@ class ProfileOne extends Component {
                     
                         <div class="INPUT-FORM1"> 
                             <p>First Name :</p>
-                            <input type="text" class="input p-2" placeholder="Enter First name"/><br></br>
+                            <input name="firstName" value={this.state.firstName}  onChange={this.handleChange} type="text" class="input p-2" placeholder="Enter First name"/><br></br>
                             <p>Last Name :</p>
-                            <input type="text" class="input p-2" placeholder="Enter Last name"/><br></br>
+                            <input name="lastName" value={this.state.lastName}  onChange={this.handleChange} type="text" class="input p-2" placeholder="Enter Last name"/><br></br>
                             <p>Username :</p>
-                            <input type="text" class="input p-2" placeholder="Enter Username"/>
+                            <input name="username" value={this.state.username}  onChange={this.handleChange} type="text" class="input p-2" placeholder="Enter Username"/>
                             {this.state.usernameError? <p className="pro-error">Username already exist!</p> : <br/>}
                             <p>Email :</p>
-                            <input type="text" class="input p-2" placeholder="Enter Email"/>
+                            <input name=" emailProfile" value={this.state. emailProfile}  onChange={this.handleChange} type="text" class="input p-2" placeholder="Enter Email"/>
                             {this.state.emailError? <p className="pro-error">Email already registered!</p> : <br/>}
                             <div class="btn btn-pro">Save</div>
                             {this.state.succeed? <p className="pro-success">Saved successfully!</p> : <br/>}
--- Front/src/components/profileThree.jsx
+++ Front/src/components/profileThree.jsx
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import './CSS/profileThree.css';
 import Select from 'react-select';
-
-
-
 const options = [
                     { value: 'java', label: 'java' },
                     { value: 'php', label: 'php' },
@@ -28,7 +25,31 @@ const customStyles = {
 }             
 
 class profileThree extends Component {
-    state = {  }
+    state = {
+        bio: null,
+        selectedIntrest:null,
+      }
+
+       handleChange = (e) => { 
+           let target=e.target;
+           let name = target.name;
+           let value = target.value
+           console.log(value)
+           this.setState({
+            [name]:value
+           });
+      }
+      /*handleChange2 = (e) => {
+        let value = Array.from(e.target.selectedIntrest, option => option.value);
+        console.log(value)
+        this.setState({values: value});
+      } */
+
+      handleUploadClick = ()=>{
+          document.getElementById("upload-file-pro").click();
+          console.log(document.getElementById("upload-file-pro"))
+          
+      }
     render() { 
         return ( 
             <React.Fragment>
@@ -37,6 +58,9 @@ class profileThree extends Component {
                             <h1 className="w-100 mb-2">Please select Subjects you are intrested in :</h1>
                             <div className="black-text w-75">
                                 <Select 
+                                name="selectedIntrest"
+                                value={this.state.selectedIntrest}
+                                onChange={this.handleChange2}
                                 styles={customStyles}
                                     isMulti
                                     name="colors"
@@ -52,8 +76,8 @@ class profileThree extends Component {
                             <div class="fileUploadInput w-100">
                             <h1 className="w-100 mb-2">please Upload your resume Files here:</h1>
                                 <div className="w-75 d-flex flex-row">
-                                    <input className="w-75" type="file" />
-                                    <button className="w-25 d-flex justify-content-center rounded-right align-items-center">
+                                    <input id="upload-file-pro" className="w-75" type="file" />
+                                    <button onClick={this.handleUploadClick} className="w-25 d-flex justify-content-center rounded-right align-items-center">
                                         <svg width="1em" height="1em" viewBox="0 0 16 16" class=" bi bi-cloud-arrow-up" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                             <path fill-rule="evenodd" d="M4.406 3.342A5.53 5.53 0 0 1 8 2c2.69 0 4.923 2 5.166 4.579C14.758 6.804 16 8.137 16 9.773 16 11.569 14.502 13 12.687 13H3.781C1.708 13 0 11.366 0 9.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383zm.653.757c-.757.653-1.153 1.44-1.153 2.056v.448l-.445.049C2.064 6.805 1 7.952 1 9.318 1 10.785 2.23 12 3.781 12h8.906C13.98 12 15 10.988 15 9.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 4.825 10.328 3 8 3a4.53 4.53 0 0 0-2.941 1.1z"/>
                                             <path fill-rule="evenodd" d="M7.646 5.146a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 6.707V10.5a.5.5 0 0 1-1 0V6.707L6.354 7.854a.5.5 0 1 1-.708-.708l2-2z"/>
@@ -63,6 +87,19 @@ class profileThree extends Component {
                             </div>
                         </div> 
                     </div>
+                    <div className="bioBox">
+                        <div>
+                            <h3>Bio :</h3>
+                        </div>
+                        <div className="bioField d-flex justify-content-center">
+                            <textarea name="bio" value={this.state.bio}  onChange={this.handleChange}  className="" maxlength="175" rows="4" cols="53">
+                                
+                            </textarea>
+                        </div>
+                    </div>
+
+                    <div class="btn btn-proo">Save</div>
+                         
             </React.Fragment>
             
          );
