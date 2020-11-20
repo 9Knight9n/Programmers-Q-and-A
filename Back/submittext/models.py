0b6ec4653ceba984e76178ddc3cf3374085990fa
sadeghjafari5528 <sadeghjafari5528@gmail.com>
2020-11-20 08:51:12 +0330

    Make Question Model

--- Back/submittext/models.py
+++ Back/submittext/models.py
@@ -3,8 +3,14 @@ from django.db import models
 from registeration.models import User
 import datetime
 # Create your models here.
-class ChatText(models.Model):
+class Answer(models.Model):
     user = models.ForeignKey(User , on_delete=models.CASCADE)
+    question = models.models.ForeignKey(Question , on_delete=models.CASCADE)
     parent_text = models.ForeignKey('self' , on_delete=models.CASCADE , null=True)
+    text = models.TextField()
+    time = models.DateTimeField(default=datetime.datetime.now())
+
+class Question(models.Model):
+    user = models.ForeignKey(User , on_delete=models.CASCADE)
     text = models.TextField()
     time = models.DateTimeField(default=datetime.datetime.now())
