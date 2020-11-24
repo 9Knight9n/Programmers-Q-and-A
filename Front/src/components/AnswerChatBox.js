e12d1149aceb4d671abe17bde410c5a50d6189ff
abed ebadi <73102057+abed-eb@users.noreply.github.com>
2020-11-24 21:24:16 +0330

    Changed options icon

--- Front/src/components/AnswerChatBox.js
+++ Front/src/components/AnswerChatBox.js
@@ -90,7 +90,9 @@ class AnswerChatBox  extends Component {
                             <div id="options" className="options ml-auto">
                                 <Dropdown>
                                     <Dropdown.Toggle className="mr-2" id="dropdown-basic">
-                                       <img className="moreOptionsImg" src={moreOptionsImg} />
+                                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-three-dots-vertical" fill="currentColor" xmlns="http://www.w3.org/2000/svg%22%3E" >
+                                            <path fill-rule="evenodd" d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
+                                    </svg>
                                     </Dropdown.Toggle>
 
                                     <Dropdown.Menu>
