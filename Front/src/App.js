0bb44c19521759f0ab42d9866b315f62d9688b36
Mohamad Sajad Naghizadeh <naghizadeh.mdsd@gmail.com>
2020-11-27 12:27:44 +0330

    Fixed editor component

--- Front/src/App.js
+++ Front/src/App.js
@@ -14,7 +14,7 @@ import SignInForm from './pages/SignInForm';
 import Homepage from './components/homepage';
 import Setting from './components/setting';
 import Cookies from 'js-cookie';
-import Texteditor from './components/texteditor';
+import TextEditor from './components/textEditor';
 
 import './index.css';
 
@@ -22,16 +22,37 @@ import './index.css';
 
 class App extends Component {
   state={
+    editorContent:null,
+    editorVisible:true,
     
   }
+
+  showEditor = () => {
+    this.setState({ editorVisible: true });
+    // console.log(this.state.submit)
+
+  };
+
+  hideEditor = () => {
+      this.setState({ editorVisible: false });
+  };
+
+  updateContent = (value) => {
+    this.setState({editorContent:value})
+  };
 
 
   render(){
     return (
       <Router>
+        {this.state.editorContent}
           <Switch>
           <Route path="/Texteditor">
-            <Texteditor />
+            <TextEditor 
+              content={this.state.content} 
+              updateContent={this.updateContent} 
+              hideEditor={this.hideEditor}
+              editorVisible={this.state.editorVisible}/>
           </Route>
           <Route path="/login">
             <SignInForm />
