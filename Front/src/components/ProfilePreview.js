ee6f9c2ca7059bb358c0c76e8a0ac16467516861
abed ebadi <73102057+abed-eb@users.noreply.github.com>
2020-12-05 20:38:44 +0330

    Added html/css to profilePreview

--- Front/src/components/ProfilePreview.js
+++ Front/src/components/ProfilePreview.js
@@ -1,36 +1,40 @@
 import React, { Component } from 'react';
 import './CSS/ProfilePreview.css';
 import profileImg from '../img/default-profile-picture.jpg'
+import exitImg from '../img/exit.png'
 
 class ProfilePreview extends Component {
     state = {  }
     render() { 
         return ( 
-            <div className="preview-main-box ">
-                <div className="preview-elements mb-3">
-                    <div  className="preview-profile">
-                        <div className="preview-profileImg d-flex justify-content-center">
-                            <img src={profileImg} />
-                            <div></div>
-                            <div className="preview-FirstName-LastName">
-                                <p className="mb-3 preview-Firstname">Sadegh jafari</p>
+            <div className="ProfilePreview-main-box ">
+                <div className="ProfilePreview-exitImg">
+                    <img src={exitImg} />
+                </div>
+                <div className="ProfilePreview-elements mb-3">
+                    <div  className="ProfilePreview-profile">
+                        <div className="ProfilePreview-headerProfile row d-flex justify-content-center">
+                            <div className="ProfilePreview-profileImg col">
+                                <img src={profileImg} />
+                            </div>
+                            <div className="ProfilePreview-FirstName-LastName-email-userName col">
+                                <div className="ProfilePreview-FirstName-LastName">
+                                    <p>Sadegh Jafari</p>
+                                </div>
+                                <div className="ProfilePreview-email-userName">
+                                    <p>email : Sadegh@yahoo.com</p>
+                                    <p>username : SadeghJafari</p>
+                                </div> 
                             </div> 
                         </div>
-                        
-                        <div className="preview-personalDetail">
+                        <div className="ProfilePreview-personalDetail">
 
-                            <label for="preview-bioBox">Bio</label>
-                            <div className="preview-bioBox">
-                                <p className="preview-bio">
+                            <label for="ProfilePreview-bioBox">Bio</label>
+                            <div className="ProfilePreview-bioBox">
+                                <p className="ProfilePreview-bio">
                                     Hi! I'm back-end developer. I'm intrested in python. Get in touch with me via email. Thanks.
                                 </p>
                             </div>
-
-                            <label for="preview-Username">Username</label>
-                                <p className="mb-3 preview-Username">SadeghJafari</p>
-
-                            <label for="preview-Email">Email Address</label>
-                                <p className="preview-Email">Sadegh@yahoo.com</p>
 
                         </div>
                     </div>
