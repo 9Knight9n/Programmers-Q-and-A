9562ae706602177d40ba7213510822dcf49b63c3
abed ebadi <73102057+abed-eb@users.noreply.github.com>
2020-12-11 14:58:53 +0330

    handled requests

--- Front/src/components/ProfilePreview.js
+++ Front/src/components/ProfilePreview.js
@@ -9,37 +9,50 @@ import  askedQImg from '../img/askedQuestion.png';
 import  answeredQImg from '../img/answeredQuestion.png';
 import groupChat from '../img/group.png';
 import ReactTooltip from 'react-tooltip';
+import {decodeList} from './util'
+
+const options = [
+    { value: 'java', label: 'java' },
+    { value: 'php', label: 'php' },
+    { value: 'python', label: 'python' },
+    { value: 'c', label: 'c' },
+    { value: 'c++', label: 'c++' },
+    { value: 'c#', label: 'c#' },
+    { value: 'html', label: 'html' },
+    { value: 'css', label: 'css' },
+    { value: 'java-script', label: 'java script' }
+]
 
 class ProfilePreview extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            first_name: 'Sadegh',
-            last_name: 'Jafari',
+            first_name: '',
+            last_name: '',
             // userEmail: 'sadegh@yahoo.com',
-            userName: 'SadeghJafari79',
+            userName: '',
             userId: null,
-            userBio: 'Hi! This is my bio. Thanks for visiting',
-            userIntrests:'Java, Python, C++, PHP, HTML, CSS',
-            askedQ: '122',
-            answeredQ: '143',
-            numOfChatroom: '20'
+            userBio: '',
+            userIntrests: [],
+            askedQ: '',
+            answeredQ: '',
+            numOfChatroom: ''
         }; 
 
     }
 
-    componentDidMount = () => {
-        if (!sessionStorage.getItem(this.props.userid + ":avatar")) {
-            await getUserAvatar(this.props.userid);  
+    componentDidMount = async () => {
+        if (!sessionStorage.getItem("15" + ":avatar")) {
+            await getUserAvatar("15");  
         }
         this.loadData();
     }
-
     loadData = async () => {
         // this.setState({loading:true})
+        console.log('entred loadData....')
         let config = {
             url:"http://127.0.0.1:8000/api/ShowUserProfile/",
-            needToken:true,
+            needToken:false,
             type:"post",
             formKey:[
                 "user_id",
@@ -51,15 +64,22 @@ class ProfilePreview extends Component {
         let data = [];
         data = await request(config);
         if (data) {
+            console.log("LOADING......")
             this.setState({
                 first_name: data.first_name,
                 last_name: data.last_name,
-                userName: data.userName,
+                userName: data.username,
                 userBio: data.description,
                 askedQ: data.askedQuestions,
-                answeredQuestions: data.answeredQuestions,
-                numOfChatroom: data.numberOfChatrooms
+                answeredQ: data.answeredQuestions,
+                numOfChatroom: data.numberOfChatrooms,
+                // userIntrests: decodeList(options, "111111111")
             });
+            let userIntrests = decodeList(options, data.interests)
+            console.log("user intersting:  ", userIntrests)
+            this.setState({
+                userIntrests
+            })
         }
         console.log(data)
         // this.setState({loading:false})
@@ -88,13 +108,13 @@ class ProfilePreview extends Component {
                                 <div className="ProfilePreview-email-userName d-flex">
                                     {/* <p>email : {this.state.userEmail}</p> */}
                                     <label for="ProfilePreview-userName-P">username : </label>
-                                    <p className="ProfilePreview-userName-P"> {this.state.userName}</p>
+                                    <p className="ProfilePreview-userName-P ml-1">{this.state.userName}</p>
                                 </div>
                                 <div className="ProfilePreview-intrestsBox">
                                     <label for="ProfilePreview-intrests">Intrested In : </label> 
                                     <div className="ProfilePreview-intrests">
                                         <p>
-                                            {this.state.userIntrests}
+                                            {this.state.userIntrests.map(ui => ui.value + "  ,  ")}
                                         </p>
                                     </div>
                                 </div>
