7785cfd49e98969e598f3bf4ef745852a44c3304
sadeghjafari5528 <sadeghjafari5528@gmail.com>
2020-12-17 19:28:30 +0330

    Restart genreral chatroom

--- Back/chatroom/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Chatroom
-
-class ShowUChatroomProfileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Chatroom
-        fields = ['selectedTopic', 'chatroomName', 'Description']
--- Back/generalchatroom/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('generalchatroom/', views.index, name='generalchatroom'),
-    path('generalchatroom/<str:room_name>/', views.room, name='generalchatroom'),
-]
