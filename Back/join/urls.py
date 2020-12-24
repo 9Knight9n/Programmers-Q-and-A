8086a427622f9cada54e83a600e61140f9c7997b
zahra salehi <zahra.salehi987777@gmail.com>
2020-12-24 19:07:11 +0330

    show_users

--- Back/join/urls.py
+++ Back/join/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path("Join/", views.Join, name="Join"),
     path("show_Users/", views.show_Users, name="show_Users"),
-    #path("ShowChatroomProfile/", views.ShowChatroomProfile, name="ShowChatroomProfile"),
+    path("Left/", views.Left, name="Left"),
 
 ]
--- Back/join/views.py
+++ Back/join/views.py
@@ -17,24 +17,40 @@ def Join(request):
     if list(user) == [] or list(chatroom) == []:
         return Response({'message': 'User or chatroom not found'})
     chatroom_user = Chatroom_User.objects.filter(user=user[0], chatroom=chatroom[0])
-    if not list(chatroom_user):
-        user = user
-        chatroom = chatroom
-        Chatroom_User.user.add(user)
-        serializer = JoinSerializer(user)
-        return Response(serializer.data)
-    return Response({'message': 'User found'})
+    if list(chatroom_user) != []:
+        return Response({'message': 'User has joined'})
+    chatroom_user = Chatroom_User.objects.create(user=user[0], chatroom=chatroom[0])
+    chatroom_user.save()
+    return Response({'message': 'New chatroom_User created'}, status=status.HTTP_201_CREATED)
+
+
+@api_view(['POST'])
+@permission_classes([])
+def Left(request):
+    data = dict(request.POST)
+    chatroom = Chatroom.objects.filter(id=request.data['chatroomId'])
+    user = User.objects.filter(id=data['id'][0])
+    if list(user) == [] or list(chatroom) == []:
+        return Response({'message': 'User or chatroom not found'})
+    chatroom_user = Chatroom_User.objects.filter(user=user[0], chatroom=chatroom[0])
+    if list(chatroom_user) == []:
+        return Response({'message': 'User has not joined'})
+    chatroom_user.delete()
+    return Response({'message': 'chatroom_User deleted'}, status=status.HTTP_201_CREATED)
 
 
 @api_view(['POST'])
 @permission_classes([])
 def show_Users(request):
-    Users = User.objects.all()
+    chatroom = Chatroom.objects.filter(id=request.data['chatroomId'])
+    chatroom_user = Chatroom_User.objects.filter(chatroom=chatroom[0])
     data = []
-    for i in range(len(Users)):
-        # data.append(ChatroomSerializer(chatrooms[i]))
-        data.append({'id': Users[i].id, 'name': Users[i]}.username)
-        print(Users[i].chatroomAvatar)
-        image = open(str(Users[i].profile_picture), 'r').read()
+    for i in range(len(chatroom_user)):
+        data.append({'id': chatroom_user[i].user.id, 'name': chatroom_user[i].user.username})
+        print(chatroom_user[i].user.profile_picture)
+        image = open(str(chatroom_user[i].user.profile_picture), 'r').read()
         data[i]['Base64'] = image
     return Response(data, status=status.HTTP_200_OK)
+
+
+
