daf8915ce5cc1269e72f2316e8115b29307de4a4
zahra salehi <zahra.salehi987777@gmail.com>
2020-12-27 21:51:42 +0330

    show_all_message

--- Back/generalchatroom/urls.py
+++ Back/generalchatroom/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('generalchatroom/', views.index, name='index'),
     path('generalchatroom/<str:room_name>/', views.room, name='room'),
+    path('show_Message/', views.show_Message, name='show_Message'),
 ]
