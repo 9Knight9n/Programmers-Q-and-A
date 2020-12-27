330c666d00b8a691bec9936541b4c16fa6f2b36c
abed ebadi <73102057+abed-eb@users.noreply.github.com>
2020-12-27 20:25:02 +0330

    Update avatar.jsx

--- Front/src/components/avatar.jsx
+++ Front/src/components/avatar.jsx
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
+import {getUserAvatar} from './util';
 
 class Avatar extends Component {
-    state = {  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            isOnline: true,
+            showAvatar: this.props.showAvatar,
+            userId: this.props.userId,
+        }; 
+
+    }
     render() { 
         return ( 
-            <p>hi</p>
+            <div className="userProfileAvatar">
+                {this.state.isOnline && this.state.showAvatar?
+                                  
+                    <StyledBadge
+                    overlap="circle"
+                    anchorOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'right',
+                    }}
+                    variant="dot"
+                    >
+                        <Avatar alt="Avatar" id="profile-img" src={getUserAvatar(this.state.userId)} />
+                    </StyledBadge> :    
+                    <img id="profile-img" src={getUserAvatar(this.state.userId)} />
+                }
+            </div>
          );
     }
 }
