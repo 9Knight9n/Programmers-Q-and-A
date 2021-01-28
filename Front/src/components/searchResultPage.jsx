0517d7541e8bd29a70989aa2952d38ed4a2eb141
Sajad <naghizadehmdsd@gmail.com>
2021-01-28 23:54:07 +0330

    fixe problems with search

--- Front/src/components/searchResultPage.jsx
+++ Front/src/components/searchResultPage.jsx
@@ -152,8 +152,8 @@ class SearchResultPage extends Component {
                                             <Link key={chatroom.ChatroomID} 
                                                 className=" m-4"
                                                 to={"/qanda"+chatroom.ChatroomID} >
-                                                <div className="d-flex flex-row result-chatroom">
-                                                    <img className="d-flex mt-auto mb-auto mr-3 ml-3" id="chatroom-img" src={chatroom.image} />
+                                                <div className="d-flex flex-row result-chatroom ">
+                                                    <img className="d-flex mt-auto mb-auto mr-3 ml-3 " id="chatroom-img" src={chatroom.image} />
                                                     <div className="d-flex mt-auto mb-auto pr-5">{chatroom.name}</div>
                                                 </div>
                                             </Link>)}
